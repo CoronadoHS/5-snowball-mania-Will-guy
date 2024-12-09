9a3cd18118ad6445befe1ceec24cea4a9e99f7d9
Will.guy <william.wilshusen@d11.org>
2024-12-09 13:29:58 -0700

    edited

--- SnowballFight.py
+++ SnowballFight.py
@@ -29,17 +29,16 @@ def main():
     print("Welcome to Snowball Mania!")
     name = input("What is your name?\n>")
     players.append(Player(name))
+    time.sleep(1)
     opponent = input(f"Welcome {name}! Who would you like to play against?\n>")
     players.append(Player(opponent))
-
+    time.sleep(1)
     add = input("Any more opponents? (Press 'ENTER' to finish)\n>")
     while(add != ""):
+        time.sleep(.5)
         players.append(Player(add))
         add = input("Any more opponents? (Press 'ENTER' to finish)\n>")
-        
-    for p in players:
-        print(p)
-
+    
     gameplay(name, players, input("Would you like to play on auto or manual?\n>"))
         
 
@@ -48,30 +47,58 @@ def main():
 
 def gameplay (name, players, manual):
     if(manual.lower().startswith("a") == True):
+        # automatic mode
         while(len(players) > 1):
             thrower = random.choice(players)
             target = random.choice(players)
-            while (target == thrower):
-                target = random.choice(players)
-            print(f"{thrower} is THROWING at {target}!")
-            
+            if(target == thrower):
+                while(target == thrower):
+                    target = random.choice(players)
+            print(f"{thrower.name} is THROWING at {target.name}!")
+            time.sleep(1.75)
             if(hitResult(thrower, target) == True):
-                print(f"{thrower.name} HIT {target.name}! {target.name} has {target.health}/100 HP left.")
+                print(f"{thrower.name} HIT {target.name}!")
+                if(target.health <= 0):
+                    print(f"{target.name} is OUT!!!")
+                    players.pop(players.index(target))
+
             else:
                 print(f"{thrower.name} missed!")
-            coinFlip(thrower, target)
+            displayHealth(players)
+            # coinFlip(thrower, target)
     else:
         while(len(players) > 1):
+            # manual mode
             thrower = random.choice(players)
-            target = players.index()
-            while (target == thrower):
+            if(thrower.name == name):
+                print("Your turn to throw!")
+                time.sleep(.5)
+                target = input("Who would you like to target?\n>")
+            else:
                 target = random.choice(players)
-            print(f"{thrower} is THROWING at {target}!")
+
+            while(target == thrower):
+                target = random.choice(players)
+            time.sleep(1.75)
+            if(thrower.name == name):
+                print(f"You are throwing at {target.name}!")
+            else:
+                print(f"{thrower.name} is throwing at {target.name}!")
+            
             if(hitResult(thrower, target) == True):
-                print(f"{thrower.name} HIT {target.name}! {target.name} has {target.health}/100 HP left.")
+                if(thrower.name == name):
+                    print(f"You HIT {target.name}!")
+                else:
+                    print(f"{thrower.name} HIT {target.name}!")
+                time.sleep(1)
+                if(target.health <= 0):
+                    print(f"{target.name} is OUT!!!")
+                    players.pop(players.index(target))
+
             else:
                 print(f"{thrower.name} missed!")
-            coinFlip(thrower, target)
+            displayHealth(players)
+            # coinFlip(thrower, target)
         
 def coinFlip(thrower, target):
     if(random.random() <= .50):
@@ -94,10 +121,17 @@ def hitResult(thrower, target):
         return False
     
 
-def displayHealth():
+def displayHealth(players):
     display = ""
     for p in players:
         display += f"{p.name} HEALTH:" 
+        x = p.health
+        while(x > 0):
+            display += "|"
+            x -= 1
+        display += "\n"
+    print(display)
+
     # based on the number that is passed in, return True or False 
     # indicating if this was a hit or a miss
 
